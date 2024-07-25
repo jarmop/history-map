477606b182fe1071532116ecbaa1ebb06b22eb18
Jarmo Pihlajaniemi <jarmo.pihlajaniemi@relexsolutions.com>
2024-07-25 11:01:50 +0300

    Initial history-map app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import map from "./assets/World_map_blank_without_borders.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <img src={map} alt="Map" />
+    </div>
+  );
+}
+
+export default App;
