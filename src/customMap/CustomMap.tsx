4faf6d0ec6261bf435a1bfe1fa5634c0b69f7abd
Jarmo Pihlajaniemi <jarmo.pihlajaniemi@relexsolutions.com>
2024-07-29 21:37:12 +0300

    Zoom and pan with viewbox

--- src/customMap/CustomMap.tsx
+++ src/customMap/CustomMap.tsx
@@ -1,14 +1,9 @@
 import italy from "./italy.png";
 import { africaBorder, asiaBorder, europeBorder } from "./continents";
 import { islandsBorders } from "./islands";
-import { cities } from "./cities";
-import { france, iberia } from "./regions";
 import { veniceCities } from "./venice";
-import { useLatLonToXy } from "./useLatLonToXy";
-import { useState } from "react";
-
-const width = 1000;
-const height = 1000;
+import { useEffect, useRef, useState } from "react";
+import { latLonTupleToXYTuple, maxHeight, maxWidth } from "./helpers";
 
 interface CityProps {
   city: number[];
@@ -16,7 +11,7 @@ interface CityProps {
 
 function City({ city }: CityProps) {
   const [x, y] = city;
-  return <circle cx={x} cy={y} r="2" />;
+  return <circle cx={x} cy={y} r="3" />;
 }
 interface BorderProps {
   border: number[][];
@@ -35,39 +30,82 @@ function Border({ border }: BorderProps) {
   );
 }
 
+const aspectRatio = 16 / 9;
+
 export function CustomMap() {
-  const [zoom, setZoom] = useState(1);
+  const [zoom, setZoom] = useState(4);
+  const [isMousedown, setMousedown] = useState(false);
+  const [xy, setXy] = useState(latLonTupleToXYTuple([60, -30]));
+  const [containerSize, setContainersize] = useState([1, 1]);
+  const domRef = useRef<HTMLDivElement>(null);
+
+  const multiplier = 10 / zoom;
+  const width = containerSize[0] * multiplier;
+  const height = containerSize[1] * multiplier;
+  const maxX = width < maxWidth ? maxWidth - width : 0;
+  const maxY = height < maxHeight ? maxHeight - height : 0;
+
+  useEffect(() => {
+    const width = domRef.current?.offsetWidth || 1;
+    setContainersize([width, width / aspectRatio]);
+  }, []);
+
+  useEffect(() => {
+    function mousedown() {
+      setMousedown(true);
+    }
+    function mousemove(e: MouseEvent) {
+      if (isMousedown) {
+        setXy((xy) => {
+          const newX = xy[0] - e.movementX * multiplier;
+          const newY = xy[1] - e.movementY * multiplier;
+          return [
+            newX < 0 ? 0 : newX > maxX ? maxX : newX,
+            newY < 0 ? 0 : newY > maxY ? maxY : newY,
+          ];
+          // return [newX, newY];
+        });
+      }
+    }
+    function mouseup() {
+      setMousedown(false);
+    }
+    document.addEventListener("mousedown", mousedown);
+    document.addEventListener("mousemove", mousemove);
+    document.addEventListener("mouseup", mouseup);
+
+    return () => {
+      document.removeEventListener("mousedown", mousedown);
+      document.removeEventListener("mousemove", mousemove);
+      document.removeEventListener("mouseup", mouseup);
+    };
+  }, [isMousedown, multiplier, setXy, maxX, maxY]);
 
   return (
     <div>
-      <div style={{ position: "fixed" }}>
-        <button onClick={() => setZoom(zoom + 1)}>+</button>
-        <button onClick={() => setZoom(zoom - 1)} disabled={zoom === 1}>
-          -
-        </button>
-      </div>
+      <div ref={domRef}>
+        <div style={{ position: "fixed" }}>
+          <button onClick={() => setZoom(zoom + 1)}>+</button>
+          <button onClick={() => setZoom(zoom - 1)} disabled={zoom === 1}>
+            -
+          </button>
+        </div>
 
-      <svg
-        // width={width}
-        // width={"auto"}
-        // height={height}
-        // height={"auto"}
-        // viewBox={`0 0 ${width} ${height}`}
-        viewBox={`0 0 4000 4000`}
-        style={{ background: "lightcyan" }}
-      >
-        <Border border={europeBorder} />
-        {/* <Border border={iberia} /> */}
-        {/* <Border border={france} /> */}
-        <Border border={africaBorder} />
-        <Border border={asiaBorder} />
-        {islandsBorders.map((border, i) => (
-          <Border key={i} border={border} />
-        ))}
-        {veniceCities.map((city, i) => (
-          <City key={i} city={city} />
-        ))}
-      </svg>
+        <svg
+          viewBox={`${xy[0]} ${xy[1]} ${width} ${height}`}
+          style={{ background: "lightcyan" }}
+        >
+          <Border border={europeBorder} />
+          <Border border={africaBorder} />
+          <Border border={asiaBorder} />
+          {islandsBorders.map((border, i) => (
+            <Border key={i} border={border} />
+          ))}
+          {veniceCities.map((city, i) => (
+            <City key={i} city={city} />
+          ))}
+        </svg>
+      </div>
       <img src={italy} alt="italy" width={500} />
     </div>
   );
--- src/customMap/helpers.ts
+++ src/customMap/helpers.ts
@@ -1,19 +1,14 @@
 import { latLonByName } from "./latLonByName";
 
-// const leftLon = -15;
-// const topLat = 60;
-
 const leftLon = -180;
 const topLat = 90;
 
-// export type Coord = {
-//   x: number;
-//   y: number;
-// };
-
 const latLonRatio = 3 / 4;
-const lonXRatio = 10;
+const lonXRatio = 20;
 const latYRatio = lonXRatio / latLonRatio;
+
+export const maxWidth = lonXRatio * 360;
+export const maxHeight = latYRatio * 180;
 
 function lonToX(lon: number) {
   return (lon - leftLon) * lonXRatio;
@@ -22,12 +17,6 @@ function lonToX(lon: number) {
 function latToY(lat: number) {
   return (topLat - lat) * latYRatio;
 }
-
-// export function latLonTupleToCoord(latLon: number[]): Coord {
-//   const lat = latLon[0];
-//   const lon = latLon[1];
-//   return { x: lonToX(lon), y: latToY(lat) };
-// }
 
 export function latLonTupleToXYTuple(latLon: number[]): number[] {
   const lat = latLon[0];
