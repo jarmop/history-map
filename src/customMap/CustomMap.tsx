90d5818a7c4435eca8d8276da53c32ad2819960a
Jarmo Pihlajaniemi <jarmo.pihlajaniemi@relexsolutions.com>
2024-07-30 14:23:10 +0300

    Zoom on target

--- src/customMap/CustomMap.tsx
+++ src/customMap/CustomMap.tsx
@@ -43,11 +43,41 @@ export function CustomMap() {
     totalHeight,
   } = useLatLonToXy(zoom, containerSize);
   const [isMousedown, setMousedown] = useState(false);
-  const [latLon, setXy] = useState([90, -180]);
+  const [latLon, setLatLon] = useState([90, -180]);
+  const [mouseOnZoom, setMouseOnZoom] = useState<
+    | {
+        clientXY: number[];
+        clientLatLon: number[];
+      }
+    | undefined
+  >(undefined);
 
   const xy = latLonTupleToXYTuple(latLon);
 
   const domRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!mouseOnZoom) {
+      return;
+    }
+    const mouseLatOnZoomDiff = 90 - mouseOnZoom.clientLatLon[0];
+    const mouseLatOnZoom = latLon[0] - mouseLatOnZoomDiff;
+
+    const mouseLonOnZoomDiff = mouseOnZoom.clientLatLon[1] + 180;
+    const mouseLonOnZoom = latLon[1] + mouseLonOnZoomDiff;
+    const mouseXYOnZoom = latLonTupleToXYTuple([
+      mouseLatOnZoom,
+      mouseLonOnZoom,
+    ]);
+
+    const newX = mouseXYOnZoom[0] - mouseOnZoom.clientXY[0];
+    const newY = mouseXYOnZoom[1] - mouseOnZoom.clientXY[1];
+
+    const newLatLon = xYTupleToLatLonTuple([newX, newY]);
+
+    setMouseOnZoom(undefined);
+    setLatLon(newLatLon);
+  }, [latLonTupleToXYTuple, xYTupleToLatLonTuple, setLatLon]);
 
   const width = containerSize[0];
   const height = containerSize[1];
@@ -66,7 +96,7 @@ export function CustomMap() {
     function mousemove(e: MouseEvent) {
       if (isMousedown) {
         const [dx, dy] = [e.movementX, e.movementY];
-        setXy((latLon) => {
+        setLatLon((latLon) => {
           const xy = latLonTupleToXYTuple(latLon);
           const newX = xy[0] - dx;
           const newY = xy[1] - dy;
@@ -80,6 +110,10 @@ export function CustomMap() {
     function wheel(e: WheelEvent) {
       e.preventDefault();
       const zoomD = -e.deltaY;
+      setMouseOnZoom({
+        clientXY: [e.clientX, e.clientY],
+        clientLatLon: xYTupleToLatLonTuple([e.clientX, e.clientY]),
+      });
       setZoom((zoom) => {
         const newZoom = zoom + zoomD;
         return newZoom > 0 && newZoom <= maxZoom ? newZoom : zoom;
@@ -103,7 +137,7 @@ export function CustomMap() {
     };
   }, [
     isMousedown,
-    setXy,
+    setLatLon,
     maxX,
     maxY,
     latLonTupleToXYTuple,
@@ -113,12 +147,12 @@ export function CustomMap() {
   return (
     <div>
       <div ref={domRef}>
-        <div style={{ position: "fixed" }}>
+        {/* <div style={{ position: "fixed" }}>
           <button onClick={() => setZoom(zoom + 1)}>+</button>
           <button onClick={() => setZoom(zoom - 1)} disabled={zoom === 1}>
             -
           </button>
-        </div>
+        </div> */}
 
         <svg
           viewBox={`${xy[0]} ${xy[1]} ${width} ${height}`}
